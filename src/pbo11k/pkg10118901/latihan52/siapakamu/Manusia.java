e0f8950f511b22036e9ac02375377ed407ad5baf
Gonior <43252085+Gonior@users.noreply.github.com>
2019-10-24 01:32:13 +0700

    Dibuat : Kamis, 24 Oktober 2019

---
+++ src/pbo11k/pkg10118901/latihan52/siapakamu/Manusia.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo11k.pkg10118901.latihan52.siapakamu;
+/**
+ * @author
+ * NAMA             : Dedi cahya
+ * NIM              : 10118901
+ * KELAS            : IF11K
+ * Deskripsi Program  : Program untuk menampilkan siapa kamu berdasarkan status konsep pewarisan class
+ */
+public class Manusia {
+    protected String nama;
+    protected int umur;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public int getUmur() {
+        return umur;
+    }
+
+    public void setUmur(int umur) {
+        this.umur = umur;
+    }
+    
+    public void siapaKamu() {
+        System.out.println("Saya Manusia");
+    }
+}
